63d48709acebe0a596c87d7ed0864e7224488a17
nyius <josephdesigned@gmail.com>
2022-04-23 11:40:09 -0400

    Final release for now, styled/responsive & main functionality

--- src/App.js
+++ src/App.js
@@ -37,38 +37,66 @@ function App() {
 	const onSubmit = async e => {
 		e.preventDefault();
 
+		setAlert({
+			message: '',
+			code: '',
+		});
+
 		if (!newTimer.days && !newTimer.hours && !newTimer.minutes && !newTimer.seconds) {
 			setAlert({
 				message: 'You need to set a timer for at least 1 second!',
 				code: 'error',
 			});
 			return;
+		} else if (!newTimer.label) {
+			setAlert({
+				message: 'You need to give your timer a name',
+				code: 'error',
+			});
+			return;
+		} else {
+			clearInterval(countdownTimer);
+			setCountdownTimer(null);
+
+			const daysToSeconds = newTimer.days * 24 * 60 * 60;
+			const hoursToSeconds = newTimer.hours * 60 * 60;
+			const minutesToSeconds = newTimer.minutes * 60;
+
+			const timerSeconds = daysToSeconds + hoursToSeconds + minutesToSeconds + newTimer.seconds;
+
+			setTimer(timerSeconds);
+
+			countdown();
+			setAlert({
+				message: 'Timer set!',
+				code: 'success',
+			});
 		}
-
-		clearInterval(countdownTimer);
-		setCountdownTimer(null);
-
-		const daysToSeconds = newTimer.days * 24 * 60 * 60;
-		const hoursToSeconds = newTimer.hours * 60 * 60;
-		const minutesToSeconds = newTimer.minutes * 60;
-
-		const timerSeconds = daysToSeconds + hoursToSeconds + minutesToSeconds + newTimer.seconds;
-
-		setTimer(timerSeconds);
-
-		countdown();
-		setAlert({
-			message: 'Timer set!',
-			code: 'success',
-		});
 	};
 
 	// timer ---------------------------------------------------------------------------------------------------//
 	const countdown = () => {
 		const intervalVar = setInterval(() => {
-			setTimer(prevState => prevState - 1);
+			setTimer(prevState => {
+				if (prevState === 0) {
+					setAlert({
+						message: `Your timer for ${newTimer.label} is up!`,
+						code: 'success',
+					});
+					resetTimer();
+					return;
+				} else {
+					return prevState - 1;
+				}
+			});
 		}, 1000);
 		setCountdownTimer(intervalVar);
+	};
+
+	const resetTimer = () => {
+		clearInterval(countdownTimer);
+		setCountdownTimer(null);
+		setTimer(0);
 	};
 
 	// Convert seconds to d/h/m/s ---------------------------------------------------------------------------------------------------//
@@ -79,9 +107,11 @@ function App() {
 		const secondsToSeconds = Math.floor(timer % 60);
 
 		return (
-			<div className="bg-base-300 w-full py-5 px-10 rounded-lg shadow-lg flex flex row justify-between items-center">
-				<p className="text-3xl font-thin  tracking-wide text-center mb-5">{newTimer.label}</p>
-				<p className="text-5xl font-bold tracking-wide text-center">
+			<div className="bg-base-300 py-5 px-10 rounded-lg gap-5 shadow-lg flex flex-col lg:flex-row justify-between items-center">
+				<p className="text-xl lg:text-3xl w-full lg:w-1/2 shrink-0 font-thin tracking-wide text-center mb-2 lg:mb-5 break-all">
+					{newTimer.label}
+				</p>
+				<p className="text-xl lg:text-5xl w-full lg:w-1/2 shrink-0 font-bold tracking-wide text-center">
 					{secondsToDays}d : {secondsToHours}hr : {secondsToMinutes}min : {secondsToSeconds}
 					sec
 				</p>
@@ -91,13 +121,6 @@ function App() {
 
 	// Display an error message ---------------------------------------------------------------------------------------------------//
 	const DisplayMessage = () => {
-		setTimeout(() => {
-			setAlert({
-				message: '',
-				code: '',
-			});
-		}, 5000);
-
 		return (
 			<>
 				{alert.code === 'error' && (
@@ -116,93 +139,116 @@ function App() {
 
 	//---------------------------------------------------------------------------------------------------//
 	return (
-		<div className="App">
-			<div className="flex flex-col justify-between items-center mx-auto w-full h-screen p-28">
-				<header>
+		<div className="App flex flex-col h-screen bg-gradient-to-r from-cyan-800 to-blue-800">
+			<div className="flex flex-col justify-center h-screen items-center mx-auto w-full p-5 lg:p-28">
+				<header className="mb-10">
+					<p className="font-black text-4xl lg:text-8xl tracking-widest text-center">TIMR</p>
+					<div className="w-full bg-base-100 h-2 lg:h-4 mb-2 lg:mb-4"></div>
 					<DisplayMessage />
 				</header>
 
 				{/* Timer */}
-				<div id="timer" className="w-3/4">
+				<div id="timer" className="w-full max-w-7xl mb-20">
 					<TimeConvert />
 				</div>
 
 				{/* New timer form */}
-				<form onSubmit={onSubmit}>
-					<div className="flex flex-row w-full gap-2 items-center justify-center">
-						<div className="form-control">
-							<label htmlFor="" className="input-group input-group-vertical input-group-lg">
-								<span>Label</span>
+				<form onSubmit={onSubmit} className="w-full max-w-7xl">
+					<div className="flex flex-col lg:flex-row w-full gap-2 items-center justify-center">
+						<div className="form-control w-full lg:w-1/5">
+							<label
+								htmlFor=""
+								className="input-group input-group-horizontal lg:input-group-vertical input-group-sm lg:input-group-lg"
+							>
+								<span className="w-2/5 lg:w-full">Label</span>
 								<input
 									type="text"
 									id="label"
 									defaultValue={label}
-									className="input input-lg w-full bg-base-200"
+									className="input input-sm lg:input-lg w-full bg-base-200"
 									placeholder="label"
 									onChange={onInputChange}
 								/>
 							</label>
 						</div>
-						<div className="form-control">
-							<label htmlFor="" className="input-group input-group-vertical input-group-lg">
-								<span>Days</span>
+						<div className="form-control w-full lg:w-1/5">
+							<label
+								htmlFor=""
+								className="input-group input-group-horizontal lg:input-group-vertical input-group-sm lg:input-group-lg"
+							>
+								<span className="w-2/5 lg:w-full">Days</span>
 								<input
 									type="number"
 									id="days"
 									min="0"
 									defaultValue={days}
-									className="input input-lg w-full bg-base-200"
+									className="input input-sm lg:input-lg w-full bg-base-200"
 									placeholder="days"
 									onChange={onInputChange}
 								/>
 							</label>
 						</div>
-						<div className="form-control">
-							<label htmlFor="" className="input-group input-group-vertical input-group-lg">
-								<span>Hours</span>
+						<div className="form-control w-full lg:w-1/5">
+							<label
+								htmlFor=""
+								className="input-group input-group-horizontal lg:input-group-vertical input-group-sm lg:input-group-lg"
+							>
+								<span className="w-2/5 lg:w-full">Hours</span>
 								<input
 									type="number"
 									id="hours"
 									min="0"
 									defaultValue={hours}
-									className="input input-lg w-full bg-base-200"
+									className="input input-sm lg:input-lg w-full bg-base-200"
 									placeholder="hours"
 									onChange={onInputChange}
 								/>
 							</label>
 						</div>
-						<div className="form-control">
-							<label htmlFor="" className="input-group input-group-vertical input-group-lg">
-								<span>Minutes</span>
+						<div className="form-control w-full lg:w-1/5">
+							<label
+								htmlFor=""
+								className="input-group input-group-horizontal lg:input-group-vertical input-group-sm lg:input-group-lg"
+							>
+								<span className="w-2/5 lg:w-full">Minutes</span>
 								<input
 									type="number"
 									id="minutes"
 									min="0"
 									defaultValue={minutes}
-									className="input input-lg w-full bg-base-200"
+									className="input input-sm lg:input-lg w-full bg-base-200"
 									placeholder="minutes"
 									onChange={onInputChange}
 								/>
 							</label>
 						</div>
-						<div className="form-control">
-							<label htmlFor="" className="input-group input-group-vertical input-group-lg">
-								<span>Seconds</span>
+						<div className="form-control w-full lg:w-1/5">
+							<label
+								htmlFor=""
+								className="input-group input-group-horizontal lg:input-group-vertical input-group-sm lg:input-group-lg"
+							>
+								<span className="w-2/5 lg:w-full">Seconds</span>
 								<input
 									type="number"
 									id="seconds"
 									min="0"
 									defaultValue={seconds}
-									className="input input-lg w-full bg-base-200"
+									className="input input-sm lg:input-lg w-full bg-base-200"
 									placeholder="seconds"
 									onChange={onInputChange}
 								/>
 							</label>
 						</div>
 					</div>
-					<button className="btn w-full btn-primary mt-5">Set new timer</button>
+					<button className="btn w-full btn-success hover:bg-base-300 hover:text-success shadow-lg mt-5">
+						Set new timer
+					</button>
 				</form>
-				<div className="footer mx-auto flex items-center justify-center">Joseph Scicluna 2022</div>
+			</div>
+			<div className="flex items-center justify-center w-full">
+				<div className="footer footer-center p-4 rounded-t-xl bg-transparent w-full max-w-7xl bg-base-300 text-base-content">
+					Joseph Scicluna 2022
+				</div>
 			</div>
 		</div>
 	);
